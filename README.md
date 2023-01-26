6b43120676223cb39e16316369beda1dbf1fa826
“Mashiad <mashiad@ualberta.ca>
2023-01-25 21:33:18 -0700

    Updated deliverables.

--- README.md
+++ README.md
@@ -19,8 +19,20 @@ server.py contains contributions from:
 * Jackson Z Chang
 * Mandy Meindersma 
 
+** Mashiad Hasan 
+
+Was prepared in consultation with the following collaborator:
+** Braeden Dirksen (bdirksen@ualberta.ca)
+
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+In the file server.py:
+# The string parsing code (lines 43-45) was adapted from StackOverFlow 
+   * Originally Answered by ansetou and David Arenburg
+   * URL: https://stackoverflow.com/questions/3368969/find-string-between-two-substrings
+
+# HTTP Headers and Responses (Status codes) (lines 52-113) were implemented in accordance with MDN Web Docs
+   * URL: https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 #  coding: utf-8 
-import socketserver
+import socketserver, os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright: 2023 Mashiad Hasan (new)
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,12 +31,86 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+        # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
+        
+        s = str(self.data) 
+    
+        # The string parsing code was adapted from StackOverFlow
+        # Originally Answered by ansetou and David Arenburg
+        # URL: https://stackoverflow.com/questions/3368969/find-string-between-two-substrings
 
-        self.request.sendall(bytearray("OK",'utf-8'))
-        # self.wfile.write("Hello Client....Got your message".encode())
+        start = "b'GET "
+        end = ' HTTP'
+        request = s[s.find(start)+len(start):s.rfind(end)]
 
+        method = s[2:5]
+
+        # HTTP Headers and Responses (Status codes) were implemented with accordance to MDN Web Docs
+        # URL: https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+        
+        if method != "GET":
+            self.request.sendall('HTTP/1.1 405 Method Not Allowed\n\n'.encode())  
+            self.request.sendall('405 Method Not Allowed'.encode())
+
+        elif request!= '/favicon.ico':
+            if not os.path.exists('www' + request):
+                self.request.sendall('HTTP/1.1 404 Not Found\n\n'.encode())
+                self.request.sendall('404 Not Found'.encode())
+                
+            
+            elif '..' in request:
+                self.request.sendall('HTTP/1.1 404 Not Found\n\n'.encode())
+                self.request.sendall('404 Not Found'.encode())
+
+            elif (('.html' in request) or ('.css' in request)):
+                try:
+                    fp = open(('www'+ request),'r' )
+                    content = fp.read()
+                    self.request.sendall(('HTTP/1.1 200 OK\n').encode())
+                    if '.html' in request:
+                        self.request.sendall('Content-Type: text/html; charset=utf-8\n\n'.encode())
+                    elif '.css' in request:
+                        self.request.sendall('Content-Type: text/css; charset=utf-8\n\n'.encode())
+                    
+                    self.request.sendall(content.encode())
+
+                except:
+                    self.request.sendall('HTTP/1.1 404 Not Found\n\n'.encode())
+                    self.request.sendall('404 Not Found'.encode())    
+
+            elif ('/' != request[-1]) :   
+                self.request.sendall('HTTP/1.1 301 Moved Permanently\n'.encode())
+                
+                request = request + '/'
+                self.request.sendall(('Location: http://127.0.0.1:8080' + request + '\n' ).encode()) # new line
+
+                try:
+                    fp = open(('www'+ request + 'index.html'),'r' )
+                    content = fp.read()
+                    self.request.sendall('Content-Type: text/html; charset=utf-8\n\n'.encode())
+                    self.request.sendall(content.encode())
+
+                except:
+                    self.request.sendall('\n\n'.encode())
+                    self.request.sendall('301 Moved Permanently'.encode())
+                
+            elif ('/' == request[-1]):
+                try:
+                    self.request.sendall('HTTP/1.1 200 OK\n'.encode())
+
+                    fp = open(('www'+ request + 'index.html'),'r' )
+                    self.request.sendall('Content-Type: text/html; charset=utf-8\n\n'.encode())
+                    content = fp.read()
+                    self.request.sendall(content.encode())
+
+                except:
+
+                    self.request.sendall('No index.html file in this directory'.encode())
+        
+            else:
+                self.request.sendall('HTTP/1.1 400 Bad Request\n\n'.encode())
+                self.request.sendall('Cannot serve this file'.encode())
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
